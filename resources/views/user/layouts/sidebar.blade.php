c2035a5b0f9036a82d4c228debff5d6979c69ac5
TigorImamHamdani <2031710058@student.polinema.ac.id>
2024-05-04 19:43:41 +0700

    update : sidebar

--- resources/views/user/layouts/sidebar.blade.php
+++ resources/views/user/layouts/sidebar.blade.php
@@ -1,24 +1,24 @@
 <ul class="navbar-nav">
     <li class="nav-item">
-        {{-- <a class="nav-link {{ request()->routeIs('admin.criterias.index') ? 'active' : '' }}" href="{{ route('admin.criterias.index') }}"> --}}
+        <a class="nav-link {{ request()->routeIs('user.weight.index') ? 'active' : '' }}" href="{{ route('user.weight.index') }}">
             <div
                 class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                 <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
             </div>
-            <span class="nav-link-text ms-1">Kriteria</span>
+            <span class="nav-link-text ms-1">Input Bobot Kriteria</span>
         </a>
     </li>
     <li class="nav-item">
-        {{-- <a class="nav-link {{ request()->routeIs('admin.input-matrix.index') ? 'active' : '' }}" href="{{ route('admin.input-matrix.index') }}"> --}}
+        <a class="nav-link {{ request()->routeIs('user.input-matrix.index') ? 'active' : '' }}" href="{{ route('user.input-matrix.index') }}">
             <div
                 class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                 <i class="ni ni-app text-info text-sm opacity-10"></i>
             </div>
-            <span class="nav-link-text ms-1">Input Data awal</span>
+            <span class="nav-link-text ms-1">Input Data Penilaian</span>
         </a>
     </li>
     <li class="nav-item">
-        {{-- <a class="nav-link {{ request()->routeIs('admin.result.index') ? 'active' : '' }}" href="{{ route('admin.result.index') }}"> --}}
+        <a class="nav-link {{ request()->routeIs('user.result.index') ? 'active' : '' }}" href="{{ route('user.result.index') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                 <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
             </div>
